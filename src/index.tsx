ae35ba736d6f45a8b7aaf31d5fcdf78f90583f89
putramaghfirah <putra.maghfirah@gmail.com>
2021-04-04 22:08:22 +0700

    🚀 token

--- src/index.tsx
+++ src/index.tsx
@@ -30,13 +30,36 @@ import reportWebVitals from 'reportWebVitals';
 import { ConnectedRouter } from 'connected-react-router';
 import { history } from 'router';
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import './locales/i18n';
 
 import 'rsuite/dist/styles/rsuite-default.css';
 
-const client = new ApolloClient({
+// initialize token user
+const httpLink = createHttpLink({
   uri: 'http://localhost:4000/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
